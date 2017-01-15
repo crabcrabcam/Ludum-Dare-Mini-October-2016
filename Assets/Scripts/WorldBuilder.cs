1dc1e37dd91cf5162fd785881d130b985be098f3
Cameron Reid <cam12win@gmail.com>
2017-01-15 17:38:03 +0000

    Small file changes and some small changes to WorldBuilder.cs

--- Assets/Scripts/WorldBuilder.cs
+++ Assets/Scripts/WorldBuilder.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System;
+
 using UnityEngine;
 
 public class WorldBuilder : MonoBehaviour {
@@ -25,9 +28,30 @@ public class WorldBuilder : MonoBehaviour {
 		}
 	}
 
-	void makeLevels(List<string> sides) 
+	void makeLevels(List<string> openSides) 
 	{
-		
+		if (openSides.Contains("up"))
+        {
+            //Get list of open bottoms
+        }
+        if (openSides.Contains("down"))
+        {
+            //Get list of open tops
+        }
+        if (openSides.Contains("left"))
+        {
+            //Get list of open rights
+        }
+        if (openSides.Contains("right"))
+        {
+            //Get list of open lefts
+        }
 	}
+
+    public List<GameObject> getLists(string filePath)
+    {
+        var prefabs = File.ReadAllLines("");
+        return Objects;
+    }
 
 }
