222b28b8efa1cdf62aaf46910718dc2c88ca06e1
Cameron Reid <cam12win@gmail.com>
2017-01-15 18:26:02 +0000

    World Builder should work now. Need to test spacing between with another tester script then move to the full version

--- Assets/Scripts/WorldBuilder.cs
+++ Assets/Scripts/WorldBuilder.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 
 public class WorldBuilder : MonoBehaviour {
 
+    public List<string> openSides;
 
 	// Use this for initialization
 	void Start () 
@@ -28,30 +29,52 @@ public class WorldBuilder : MonoBehaviour {
 		}
 	}
 
-	void makeLevels(List<string> openSides) 
+	void makeLevels()
 	{
 		if (openSides.Contains("up"))
         {
-            //Get list of open bottoms
+            //Check bottom is clear
+            if (Physics.CheckSphere(new Vector2(transform.position.x, transform.position.y + 12), 0.1f))
+            {
+                //Returns true if something is there
+
+                //Get list of open bottoms
+                getList("Assets/Room Lists/bottom.txt");
+                //Make a random number from the list
+                int newRoomIndex = new System.Random().Next(0, prefabObjects.Count);
+                //Get that object
+                GameObject newRoom = prefabObjects[newRoomIndex];
+                //Place it in the world in the correct position
+                Instantiate(newRoom, new Vector3(transform.position.x, transform.position.y + 12, transform.position.z), Quaternion.identity);
+            }
+            
         }
         if (openSides.Contains("down"))
         {
             //Get list of open tops
+            getList("Assets/Room Lists/top.txt");
         }
         if (openSides.Contains("left"))
         {
             //Get list of open rights
+            getList("Assets/Room Lists/right.txt");
         }
         if (openSides.Contains("right"))
         {
             //Get list of open lefts
+            getList("Assets/Room Lists/left.txt");
         }
 	}
 
-    public List<GameObject> getLists(string filePath)
+    List<GameObject> prefabObjects;
+    void getList(string filePath)
     {
-        var prefabs = File.ReadAllLines("");
-        return Objects;
+        var prefabs = File.ReadAllLines(filePath);
+        foreach (var prefab in prefabs)
+        {
+            GameObject newPrefab = Resources.Load(prefab, typeof(GameObject)) as GameObject;
+            prefabObjects.Add(newPrefab);
+        }
     }
 
 }
