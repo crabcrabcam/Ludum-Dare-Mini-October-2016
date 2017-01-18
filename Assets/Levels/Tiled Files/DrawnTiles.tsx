0c536b25bff2a4b28e5cc27540388cfb4f3f9165
Cameron Reid <cam12win@gmail.com>
2017-01-18 13:02:57 +0000

    Added new room and moved everything from the tiled files that were in the wrong place
    On old build of the game with different name. Really should delete that :(

---
+++ Assets/Levels/Tiled Files/DrawnTiles.tsx
@@ -0,0 +1,102 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="DrawnTiles" tilewidth="512" tileheight="512" tilecount="24" columns="8">
+ <image source="../../../../RougeLike-Platformer/Assets/Art/DrawnBlockSheet.png" width="4096" height="1536"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="19" y="93" width="496" height="420"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" x="-8" y="93" width="527" height="424"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="1" x="-6" y="93" width="494" height="423"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <object id="1" x="27" y="93" width="493" height="393"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index">
+   <object id="1" x="-18" y="94" width="544" height="404"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index">
+   <object id="1" x="-5" y="94" width="503" height="393"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index">
+   <object id="1" x="-7" y="-7" width="523" height="526"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index">
+   <object id="1" x="20" y="-11" width="509" height="527"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index">
+   <object id="1" x="-13" y="-13" width="501" height="536"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index">
+   <object id="1" x="17" y="-10" width="503" height="495"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index">
+   <object id="1" x="-7" y="-13" width="521" height="497"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index">
+   <object id="2" x="-4" y="-6" width="490" height="488"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index">
+   <object id="1" x="22" y="97" width="462" height="419"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index">
+   <object id="1" x="20" y="-22" width="462" height="507"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index">
+   <object id="1" x="16" y="-18" width="466" height="552"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index">
+   <object id="1" x="13" y="-4" width="459" height="489"/>
+   <object id="2" x="449" y="55" width="71" height="429"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index">
+   <object id="1" x="36" y="-20" width="449" height="502"/>
+   <object id="2" x="-9" y="52" width="57" height="431"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index">
+   <object id="1" x="-13" y="96" width="532" height="381"/>
+   <object id="2" x="-3" y="478" width="471" height="49"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index">
+   <object id="1" x="24" y="93" width="468" height="400"/>
+  </objectgroup>
+ </tile>
+</tileset>
