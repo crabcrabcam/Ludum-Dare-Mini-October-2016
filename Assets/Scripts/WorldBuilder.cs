c1a2d9cfe740add68ad812233fc054ee8dbe5753
Cameron Reid <cam12win@gmail.com>
2017-01-19 19:28:14 +0000

    Updated world builder. Currently not working
    Ignores whatever is there already and always builds the room. Also updated player movement to ignore the room trigger when seeing if the player can stand, updated character prefab with needed things

--- Assets/Scripts/WorldBuilder.cs
+++ Assets/Scripts/WorldBuilder.cs
@@ -9,8 +9,12 @@ public class WorldBuilder : MonoBehaviour {
 
     public List<string> openSides;
 
-	// Use this for initialization
-	void Start () 
+    List<GameObject> prefabObjects = new List<GameObject>();
+
+    System.Random random = new System.Random();
+
+    // Use this for initialization
+    void Start () 
 	{
 		
 	}
@@ -25,7 +29,10 @@ public class WorldBuilder : MonoBehaviour {
 	{
 		if (other.tag == "Player") 
 		{
-			
+            print("Player!");
+            makeLevels();
+            //Destroy(gameObject.GetComponent<Collider2D>());
+            Destroy(this);
 		}
 	}
 
@@ -34,62 +41,76 @@ public class WorldBuilder : MonoBehaviour {
 		if (openSides.Contains("up"))
         {
             //Check bottom is clear
-            if (Physics.CheckSphere(new Vector2(transform.position.x, transform.position.y + 12), 0.1f))
+            //Returns true if something is there
+            if (!Physics.CheckSphere(new Vector2(transform.position.x, transform.position.y), 1f, 6, QueryTriggerInteraction.Collide))
             {
-                //Returns true if something is there
-
                 //Get list of open bottoms
                 getList("Assets/Room Lists/bottom.txt");
                 //Make a random number from the list
-                int newRoomIndex = new System.Random().Next(0, prefabObjects.Count);
+                int newRoomIndex = random.Next(0, prefabObjects.Count);
                 //Get that object
                 GameObject newRoom = prefabObjects[newRoomIndex];
                 //Place it in the world in the correct position
-                Instantiate(newRoom, new Vector3(transform.position.x - 8, transform.position.y - 5, transform.position.z), Quaternion.identity);
+                Instantiate(newRoom, new Vector3(transform.position.x - 8, transform.position.y + 15, transform.position.z), Quaternion.identity);
             }
             
         }
-        if (openSides.Contains("down"))
-        {
-            //Get list of open tops
-            getList("Assets/Room Lists/top.txt");
-            //Make a random number from the list
-            int newRoomIndex = new System.Random().Next(0, prefabObjects.Count);
-            //Get that object
-            GameObject newRoom = prefabObjects[newRoomIndex];
-            //Place it in the world in the correct position
-            Instantiate(newRoom, new Vector3(transform.position.x - 8, transform.position.y + 15, transform.position.z), Quaternion.identity);
-        }
-        if (openSides.Contains("left"))
-        {
-            //Get list of open rights
-            getList("Assets/Room Lists/right.txt");
-            //Make a random number from the list
-            int newRoomIndex = new System.Random().Next(0, prefabObjects.Count);
-            //Get that object
-            GameObject newRoom = prefabObjects[newRoomIndex];
-            //Place it in the world in the correct position
-            Instantiate(newRoom, new Vector3(transform.position.x + 8, transform.position.y + 5, transform.position.z), Quaternion.identity);
-        }
-        if (openSides.Contains("right"))
-        {
-            //Get list of open lefts
-            getList("Assets/Room Lists/left.txt");
-            //Make a random number from the list
-            int newRoomIndex = new System.Random().Next(0, prefabObjects.Count);
-            //Get that object
-            GameObject newRoom = prefabObjects[newRoomIndex];
-            //Place it in the world in the correct position
-            Instantiate(newRoom, new Vector3(transform.position.x - 24, transform.position.y + 5, transform.position.z), Quaternion.identity);
-        }
+        //if (openSides.Contains("down"))
+        //{
+        //    if (!Physics.CheckSphere(new Vector2(transform.position.x, transform.position.y - 10), 0.1f))
+        //    {
+        //        //Get list of open tops
+        //        getList("Assets/Room Lists/top.txt");
+        //        //Make a random number from the list
+        //        int newRoomIndex = random.Next(0, prefabObjects.Count);
+        //        //Get that object
+        //        GameObject newRoom = prefabObjects[newRoomIndex];
+        //        //Place it in the world in the correct position
+        //        Instantiate(newRoom, new Vector3(transform.position.x - 8, transform.position.y - 5, transform.position.z), Quaternion.identity);
+        //    }
+
+        //}
+        //if (openSides.Contains("left"))
+        //{
+        //    if (!Physics.CheckSphere(new Vector2(transform.position.x - 16, transform.position.y), 0.1f))
+        //    {
+        //        //Get list of open rights
+        //        getList("Assets/Room Lists/right.txt");
+        //        //Make a random number from the list
+        //        int newRoomIndex = random.Next(0, prefabObjects.Count);
+        //        //Get that object
+        //        GameObject newRoom = prefabObjects[newRoomIndex];
+        //        //Place it in the world in the correct position
+        //        Instantiate(newRoom, new Vector3(transform.position.x + 8, transform.position.y + 5, transform.position.z), Quaternion.identity);
+        //    }
+
+        //}
+        //if (openSides.Contains("right"))
+        //{
+        //    if (!Physics.CheckSphere(new Vector2(transform.position.x + 16, transform.position.y), 0.1f))
+        //    {
+        //        //Get list of open lefts
+        //        getList("Assets/Room Lists/left.txt");
+        //        //Make a random number from the list
+        //        int newRoomIndex = random.Next(0, prefabObjects.Count);
+        //        //Get that object
+        //        GameObject newRoom = prefabObjects[newRoomIndex];
+        //        //Place it in the world in the correct position
+        //        Instantiate(newRoom, new Vector3(transform.position.x - 24, transform.position.y + 5, transform.position.z), Quaternion.identity);
+        //    }
+
+        //}
 	}
 
-    List<GameObject> prefabObjects;
+    
     void getList(string filePath)
     {
+        print(filePath);
+        prefabObjects.Clear();
         var prefabs = File.ReadAllLines(filePath);
         foreach (var prefab in prefabs)
         {
+            print(prefab);
             GameObject newPrefab = Resources.Load(prefab, typeof(GameObject)) as GameObject;
             prefabObjects.Add(newPrefab);
         }
